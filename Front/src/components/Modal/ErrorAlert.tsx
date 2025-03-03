6a22d6612f34c558f36395372eb05359f3fb6d81
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-03-03 11:23:22 +0100

    *remove* front / excluding a deprecated version of the modale

--- Front/src/components/Modal/ErrorAlert.tsx
+++
@@ -1,7 +0,0 @@
-export default function ErrorAlert({errorMessage} : {errorMessage : string}){
-    return(
-        <div style={{margin:'0 auto', padding : '1.5rem 0'}}>
-            Error : {errorMessage}
-        </div>
-    )
-}
